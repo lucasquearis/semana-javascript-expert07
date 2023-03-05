cb7b7912717eeebc8c709bcf9dc00d77986cfdf0
lucasquearis <lucasquearis@hotmail.com>
2023-03-05 11:41:44 -0300

    feat: desafio 02 concluido

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ Marque esse projeto com uma estrela 🌟
 ### Desafios
 
 - [x] - Aula 01 - Diferenciar piscada de olhos entre olho direito e esquerdo e atualizar log para mostrar qual olho que piscou.
-- [ ] - Aula 02 - Reconhecer gestos de mãos individuais e printar no log
+- [x] - Aula 02 - Reconhecer gestos de mãos individuais e printar no log
 - [ ] - Aula 03 - Corrigir Banner de titulo de video, para ficar atrás do desenho das mãos e se tornar clicável
 - [ ] - Aula 04 - Usar as mãos virtuais também no Video Player
 
--- src/pages/titles/src/services/handGestureService.js
+++ src/pages/titles/src/services/handGestureService.js
@@ -1,15 +1,21 @@
 export default class HandGestureService {
-  #gestureEstimator
-  #handPoseDetection
-  #handsVersion
-  #detector = null
-  #gestureStrings
+  #gestureEstimator;
+  #handPoseDetection;
+  #handsVersion;
+  #detector = null;
+  #gestureStrings;
 
-  constructor({ fingerpose, handPoseDetection, handsVersion, gestureStrings, knownGestures }) {
-    this.#gestureEstimator = new fingerpose.GestureEstimator(knownGestures)
-    this.#handPoseDetection = handPoseDetection
-    this.#handsVersion = handsVersion
-    this.#gestureStrings = gestureStrings
+  constructor({
+    fingerpose,
+    handPoseDetection,
+    handsVersion,
+    gestureStrings,
+    knownGestures,
+  }) {
+    this.#gestureEstimator = new fingerpose.GestureEstimator(knownGestures);
+    this.#handPoseDetection = handPoseDetection;
+    this.#handsVersion = handsVersion;
+    this.#gestureStrings = gestureStrings;
   }
 
   async estimate(keypoints3D) {
@@ -17,55 +23,55 @@ export default class HandGestureService {
       this.#getLandMarksFromKeypoints(keypoints3D),
       // porcentagem de confiança do gesto (90%)
       9
-    )
-    return predictions.gestures
+    );
+    return predictions.gestures;
   }
 
-  async * detectGestures(predictions) {
-
+  async *detectGestures(predictions) {
     for (const hand of predictions) {
-      if (!hand.keypoints3D) continue
+      if (!hand.keypoints3D) continue;
 
-      const gestures = await this.estimate(hand.keypoints3D)
-      if (!gestures.length) continue
+      const gestures = await this.estimate(hand.keypoints3D);
+      if (!gestures.length) continue;
 
-      const result = gestures.reduce(
-        (previous, current) => (previous.score > current.score) ? previous : current
-      )
-      const { x, y } = hand.keypoints.find(keypoint => keypoint.name === 'index_finger_tip')
-      yield { event: result.name, x, y }
-
-      console.log('detected', this.#gestureStrings[result.name])
+      const result = gestures.reduce((previous, current) =>
+        previous.score > current.score ? previous : current
+      );
+      const { x, y } = hand.keypoints.find(
+        (keypoint) => keypoint.name === "index_finger_tip"
+      );
+      yield { event: result.name, x, y };
+      console.log("detected", this.#gestureStrings[result.name]);
     }
   }
 
   #getLandMarksFromKeypoints(keypoints3D) {
-    return keypoints3D.map(keypoint =>
-      [keypoint.x, keypoint.y, keypoint.z]
-    )
+    return keypoints3D.map((keypoint) => [keypoint.x, keypoint.y, keypoint.z]);
   }
 
   async estimateHands(video) {
     return this.#detector.estimateHands(video, {
-      flipHorizontal: true
-    })
+      flipHorizontal: true,
+    });
   }
 
   async initializeDetector() {
-    if (this.#detector) return this.#detector
+    if (this.#detector) return this.#detector;
 
     const detectorConfig = {
-      runtime: 'mediapipe', // or 'tfjs',
-      solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${this.#handsVersion}`,
+      runtime: "mediapipe", // or 'tfjs',
+      solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${
+        this.#handsVersion
+      }`,
       // full é o mais pesado e o mais preciso
-      modelType: 'lite',
+      modelType: "lite",
       maxHands: 2,
-    }
+    };
     this.#detector = await this.#handPoseDetection.createDetector(
       this.#handPoseDetection.SupportedModels.MediaPipeHands,
       detectorConfig
-    )
+    );
 
-    return this.#detector
+    return this.#detector;
   }
 }
